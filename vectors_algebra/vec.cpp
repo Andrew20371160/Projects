cf6752426ef9152baa5108961c37f8113f776c87
Andrew Karam Saber <androw.20371160@eng.aun.edu.eg>
2024-03-22 22:33:32 +0200

    Update vec.cpp

--- vectors_algebra/vec.cpp
+++ vectors_algebra/vec.cpp
@@ -697,5 +697,66 @@ bool matrix ::is_orthogonal(void){
     trans_mat = *this *trans_mat ;
     return trans_mat.is_identity() ;
 }
+// Check if this matrix is nilpotent
+bool matrix :: is_nilpotent(void){
+    if(is_square()){
+        //keep multiplying the matrix by itself untill the nth power
+        matrix mat_pow = *this ;
+        matrix zeroes(rows,cols,0) ;
+        for(int i = 0 ; i<rows ; i++){
+            if(mat_pow==zeroes){
+                return true ;
+            }
+            else{
+                mat_pow =  mat_pow*mat_pow ;
+            }
+        }
+        return false ;
+    }
+    return false ;
+}
 
+// Check if this matrix is involutory
+bool matrix :: is_involutory(void){
+    if(is_square()){
+        return inverse() ==*this  ;
+    }
+    return false ;
+}
 
+void matrix:: lu_fact(matrix&lower_fact,matrix&upper_fact) {
+    //first check if its square matrix
+    if(is_square()){
+        //the lower_fact matrix is the identity matrix (at first) in which we store
+        // the constants during the gaussian elimination of the original matrix
+        //after finisning the gaussian elimination the upper_fact is finished
+        lower_fact.identity() ;
+        //copy original matrix into upper_fact to performa gaussian elimination on it
+        upper_fact = *this  ;
+        for(int up_r = 0;up_r<rows-1; up_r++){
+            for(int low_r = up_r+1; low_r<rows; low_r++){
+            //check first if upper element is not zero
+                if(upper_fact.vec[up_r][up_r]==0){
+                    //partial pivoting will be added later
+                    cout<<"zero encountered at the main diagoanl use partial pivoting";
+                    return  ;
+                }
+                else{
+                    //the constant we calculate
+                    float c = -1*(upper_fact.vec[low_r][up_r]/upper_fact.vec[up_r][up_r]);
+                    //first record it into the lower_fact matrix at its position
+                    lower_fact.vec[low_r][up_r]  = -1* c ;
+                    for(int i =0 ; i<cols;i++){
+                        //then continue the gaussian elimination
+                        upper_fact.vec[low_r][i]+=c*upper_fact.vec[up_r][i] ;
+                    }
+                }
+            }
+        }
+    }
+else{
+    cout<<square_error;
+    lower_fact =  matrix(1,1,-1) ;
+    upper_fact =  matrix(1,1,-1) ;
+}
+}
