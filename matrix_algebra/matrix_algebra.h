dbf086fa66cfe518cebe70acf40d00450c31402d
Andrew Karam Saber <androw.20371160@eng.aun.edu.eg>
2024-04-04 17:04:54 +0200

    Update matrix_algebra.h

--- matrix_algebra/matrix_algebra.h
+++ matrix_algebra/matrix_algebra.h
@@ -185,7 +185,7 @@ public:
     // Calculates the dimension of the null space (kernel) of the matrix.
     // The null space consists of all vectors that are mapped to the zero vector by the linear transformation represented by the matrix.
     int dim_null_cols(void);//tested
-    int dim_null_rows(void);
+    int dim_null_rows(void);//tested
     // Calculates the dimension of the column space (range) of the matrix.
     // The column space consists of all possible linear combinations of the column vectors in the matrix.
     int dim(void);//tested
@@ -195,7 +195,7 @@ public:
     // Returns a set of vectors (as a matrix) that forms a basis for the vector space of the given dimension.
     matrix basis_cols(void);//tested
     // Returns a set of vectors (as a matrix) that forms a basis for the vector space of the given dimension.
-    matrix basis_rows(void);
+    matrix basis_rows(void);//tested
     matrix null_rows(void) ;
     matrix null_cols(void) ;
 
